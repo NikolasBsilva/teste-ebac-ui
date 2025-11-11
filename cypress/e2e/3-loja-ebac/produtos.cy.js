1add4591a5ccede81d7f2f6e03a86bb6ccecad42
Nikolas Silva <nikolasbezerradasilva9@gmail.com>
2025-11-10 22:28:11 -0300

    utilizando massa de dados para a busca de produtos

--- cypress/e2e/3-loja-ebac/produtos.cy.js
+++ cypress/e2e/3-loja-ebac/produtos.cy.js
@@ -14,17 +14,38 @@ describe('Funcionalidade: produtos', () => {
        cy.get('.woocommerce-product-details__short-description > p').should('exist')
     });
 
-    it.only('Deve buscar um produto com sucesso', () => {
+    it('Deve buscar um produto com sucesso', () => {
         let pruduto = "Vulcan Weightlifting Tank"
         produtosPage.buscarProduto(pruduto)
         cy.get('.product_title').should('contain', 'Vulcan Weightlifting Tank')
     });
 
     it('Deve visitar a página do prudoto', () => {
-        
+        produtosPage.visitarProduto('apollo running short')
+         cy.get('.product_title').should('contain', 'Apollo Running Short')
     });
 
     it('Deve adicionar produto ao carrinho ', () => {
+        let qtd = 7
+        produtosPage.buscarProduto('Typhon Performance Fleece-lined Jacket')
+        produtosPage.addProdutoCarrinho('XL', 'Black', qtd);
+        cy.get('.woocommerce-message').should('contain', qtd + ' “Typhon Performance Fleece-lined Jacket” foram adicionados no seu carrinho.')
+        
+    });
+
+    it.only('Buscando da massa de dados ', () => {
+
+        cy.fixture('produtos').then(dados => {
+            
+        produtosPage.buscarProduto(dados[0].nomeProduto)
+        produtosPage.addProdutoCarrinho(
+             dados[0].tamano,
+             dados[0].cor, 
+             dados[0].quantidade);
+        cy.get('.woocommerce-message').should('contain', dados[0].nomeProduto)
+        
+        })
+
         
     });
 });
--- cypress/support/Page-objects/produtos.page.js
+++ cypress/support/Page-objects/produtos.page.js
@@ -16,11 +16,18 @@ class ProdutosPage {
         .click()
     }
 
-    visitarProduto() {
+    visitarProduto(nomeProduto) {
+        //cy.visit(`produtos/${nomeProduto}`)
+        const urlFormatada = nomeProduto.replace(/ /g, '-')
+        cy.visit(`produtos/${urlFormatada}`)
 
     }
 
-    addProdutoCarrinho() {
+    addProdutoCarrinho(tamanho, cor, quantidade, add) {
+        cy.get('.button-variable-item-' + tamanho).click();
+        cy.get(`.button-variable-item-${cor}`).click();
+        cy.get('.input-text').clear().type(quantidade);
+        cy.get('.single_add_to_cart_button').click(add)
 
     }
 }
